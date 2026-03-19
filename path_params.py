d6d82449b9a4ec9169886cfa38961777120fa9cc
kamlesh thavani <kamlesh.simbanic2022@gmaill.com>
2026-03-19 17:56:04 +0530

    Path params

---
+++ path_params.py
@@ -0,0 +1,92 @@
+from enum import Enum
+
+from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+app = FastAPI()
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+items = [
+    {"id": "1", "name": "milk", "price": "100"},
+    {"id": "2", "name": "bread", "price": "50"},
+    {"id": "3", "name": "milk", "price": "100"},
+]
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/items")
+async def root():
+    return {"items": items}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    # return {"item_id": item_id}
+    print(item_id)
+    result = next((item for item in items if item["id"] == item_id), None)
+    print(result)
+    if result is None:
+        return {"message": "Item not found"}
+    return result
+
+
+@app.get("/users")
+async def read_users():
+    return ["Rick", "Morty"]
+
+
+@app.get("/users")
+async def read_users2():
+    return ["Bean", "Elfo"]
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
